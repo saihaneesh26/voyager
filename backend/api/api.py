cd4cbae6a31ae55a031033e942219e2470658a02
Haneesh <haneesh@Haneeshs-MacBook-Pro.local>
2026-05-31 13:20:40 +0530

    move requirements file and fix api

--- backend/api/api.py
+++ backend/api/api.py
@@ -21,11 +21,13 @@ def home():
 def invokeAI(query: str, model: str, session:str):
     result = main.getResult(query, model, session)
 
-    # message = result["messages"][-1]
+    message = result["messages"][-1]
 
-    # if isinstance(message.content, list):
-    #     text = message.content[0]["text"]
-    # else:
-    #     text = str(message.content)
+    if isinstance(message.content, list):
+        text = message.content[0]["text"]
+    else:
+        text = str(message.content)
 
-    return PlainTextResponse(result)
+    return PlainTextResponse(text)
+
+    
